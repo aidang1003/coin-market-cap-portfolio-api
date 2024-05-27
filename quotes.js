4c86dc2229eca15dfb63f107919eaf2e1b5f56df
aidang1003 <60905689+aidang1003@users.noreply.github.com>
2024-05-26 20:22:00 -0600

    Working prototype
    Working project, but want to optimize the code base. Improve the console returns. Finish integration with the main portfolio xlsx.

--- quotes.js
+++ quotes.js
@@ -8,10 +8,11 @@ const filePath = process.env.PORTFOLIO_PATH;
 const baseUrl = process.env.API_BASE_URL;
 
 class Asset {
-  constructor(assetName, assetId, assetPrice) {
+  constructor(assetName, assetId, data) {
     this.assetName = assetName;
     this.assetId = assetId;
-    this.assetPrice - assetPrice;
+    this.data = data;
+    this.assetPrice = data[assetId].quote.USD.price;
   }
 }
 
@@ -19,8 +20,7 @@ async function getDataObject() {
   try {
     const response = await axios.get(baseUrl, {
       params: {
-        // id: "11841,3794,5805,1697,1,28324,13855,1027,3773,7080,29241,6719,5665,7226,24686,1975,3890,6535,3911,11840,12220,5026,9481,1974,15188,15060,5690,29974,5604,5426,8085,22691,6758,25147,22861,3408,29470,2396,328", // All IDs od cryptos to update price on
-        id: "1",
+        id: "11841,3794,5805,1697,1,28324,13855,1027,3773,7080,29241,6719,5665,7226,24686,1975,3890,6535,3911,11840,12220,5026,9481,1974,15188,15060,5690,29974,5604,5426,8085,22691,6758,25147,22861,3408,29470,2396,328", // All IDs od cryptos to update price on
         convert: "USD",
       },
       headers: {
@@ -29,91 +29,91 @@ async function getDataObject() {
     });
     if (response) {
       // success
-      data = response.data.data["1"];
-      price = data.quote.USD.price;
+      apiData = response.data.data;
+      price = apiData["1"].quote.USD.price;
 
       // Create Asset object for all cryptocurrencies
-      // const ARB = new Asset("ARB", "11841", data);
-      // const ATOM = new Asset("ATOM", "3794", data);
-      // const AVAX = new Asset("AVAX", "5805", data);
-      // const BAT = new Asset("BAT", "1697", data);
-      const BTC = new Asset("BTC", "1", price);
-      // const DYDX = new Asset("DYDX", "28324", data);
-      // const ENS = new Asset("ENS", "13855", data);
-      // const ETH = new Asset("ETH", "1027", data);
-      // const FET = new Asset("FET", "3773", data);
-      // const GALA = new Asset("GALA", "7080", data);
-      // const GME = new Asset("GME", "29241", data);
-      // const GRT = new Asset("GRT", "6719", data);
-      // const HNT = new Asset("HNT", "5665", data);
-      // const INJ = new Asset("INJ", "7226", data);
-      // const IOT = new Asset("IOT", "24686", data);
-      // const LINK = new Asset("LINK", "1975", data);
-      // const MATIC = new Asset("MATIC", "3890", data);
-      // const NEAR = new Asset("NEAR", "6535", data);
-      // const OCEAN = new Asset("OCEAN", "3911", data);
-      // const OP = new Asset("OP", "11840", data);
-      // const OSMO = new Asset("OSMO", "12220", data);
-      // const OXT = new Asset("OXT", "5026", data);
-      // const PENDLE = new Asset("PENDLE", "9481", data);
-      // const PRO = new Asset("PRO", "1974", data);
-      // const RADAR = new Asset("RADAR", "15188", data);
-      // const rETH = new Asset("rETH", "15060", data);
-      // const RNDR = new Asset("RNDR", "5690", data);
-      // const rswETH = new Asset("rswETH", "29974", data);
-      // const SCRT = new Asset("SCRT", "5604", data);
-      // const SOL = new Asset("SOL", "5426", data);
-      // const stETH = new Asset("stETH", "8085", data);
-      // const STRK = new Asset("STRK", "22691", data);
-      // const SUSHI = new Asset("SUSHI", "6758", data);
-      // const swETH = new Asset("swETH", "25147", data);
-      // const TIA = new Asset("TIA", "22861", data);
-      // const USDC = new Asset("USDC", "3408", data);
-      // const USDE = new Asset("USDE", "29470", data);
-      // const wETH = new Asset("wETH", "2396", data);
-      // const XMR = new Asset("XMR", "328", data);
+      const ARB = new Asset("ARB", "11841", apiData);
+      const ATOM = new Asset("ATOM", "3794", apiData);
+      const AVAX = new Asset("AVAX", "5805", apiData);
+      const BAT = new Asset("BAT", "1697", apiData);
+      const BTC = new Asset("BTC", "1", apiData);
+      const DYDX = new Asset("DYDX", "28324", apiData);
+      const ENS = new Asset("ENS", "13855", apiData);
+      const ETH = new Asset("ETH", "1027", apiData);
+      const FET = new Asset("FET", "3773", apiData);
+      const GALA = new Asset("GALA", "7080", apiData);
+      const GME = new Asset("GME", "29241", apiData);
+      const GRT = new Asset("GRT", "6719", apiData);
+      const HNT = new Asset("HNT", "5665", apiData);
+      const INJ = new Asset("INJ", "7226", apiData);
+      const IOT = new Asset("IOT", "24686", apiData);
+      const LINK = new Asset("LINK", "1975", apiData);
+      const MATIC = new Asset("MATIC", "3890", apiData);
+      const NEAR = new Asset("NEAR", "6535", apiData);
+      const OCEAN = new Asset("OCEAN", "3911", apiData);
+      const OP = new Asset("OP", "11840", apiData);
+      const OSMO = new Asset("OSMO", "12220", apiData);
+      const OXT = new Asset("OXT", "5026", apiData);
+      const PENDLE = new Asset("PENDLE", "9481", apiData);
+      const PRO = new Asset("PRO", "1974", apiData);
+      const RADAR = new Asset("RADAR", "15188", apiData);
+      const rETH = new Asset("rETH", "15060", apiData);
+      const RNDR = new Asset("RNDR", "5690", apiData);
+      const rswETH = new Asset("rswETH", "29974", apiData);
+      const SCRT = new Asset("SCRT", "5604", apiData);
+      const SOL = new Asset("SOL", "5426", apiData);
+      const stETH = new Asset("stETH", "8085", apiData);
+      const STRK = new Asset("STRK", "22691", apiData);
+      const SUSHI = new Asset("SUSHI", "6758", apiData);
+      const swETH = new Asset("swETH", "25147", apiData);
+      const TIA = new Asset("TIA", "22861", apiData);
+      const USDC = new Asset("USDC", "3408", apiData);
+      const USDE = new Asset("USDE", "29470", apiData);
+      const wETH = new Asset("wETH", "2396", apiData);
+      const XMR = new Asset("XMR", "328", apiData);
 
       // Create a list of assets
       const Assets = [
-        //   ARB,
-        //   ATOM,
-        //   AVAX,
-        //   BAT,
+        ARB,
+        ATOM,
+        AVAX,
+        BAT,
         BTC,
-        //   DYDX,
-        //   ENS,
-        //   ETH,
-        //   FET,
-        //   GALA,
-        //   GME,
-        //   GRT,
-        //   HNT,
-        //   INJ,
-        //   IOT,
-        //   LINK,
-        //   MATIC,
-        //   NEAR,
-        //   OCEAN,
-        //   OP,
-        //   OSMO,
-        //   OXT,
-        //   PENDLE,
-        //   PRO,
-        //   RADAR,
-        //   rETH,
-        //   RNDR,
-        //   rswETH,
-        //   SCRT,
-        //   SOL,
-        //   stETH,
-        //   STRK,
-        //   SUSHI,
-        //   swETH,
-        //   TIA,
-        //   USDC,
-        //   USDE,
-        //   wETH,
-        //   XMR,
+        DYDX,
+        ENS,
+        ETH,
+        FET,
+        GALA,
+        GME,
+        GRT,
+        HNT,
+        INJ,
+        IOT,
+        LINK,
+        MATIC,
+        NEAR,
+        OCEAN,
+        OP,
+        OSMO,
+        OXT,
+        PENDLE,
+        PRO,
+        RADAR,
+        rETH,
+        RNDR,
+        rswETH,
+        SCRT,
+        SOL,
+        stETH,
+        STRK,
+        SUSHI,
+        swETH,
+        TIA,
+        USDC,
+        USDE,
+        wETH,
+        XMR,
       ];
 
       // Write to Excel
